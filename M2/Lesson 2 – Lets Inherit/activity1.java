7980550d5a7e701b51c7591ffac3785f23dd0e8f
rajlaxmicodingal-design <rajlaxmicodingal@gmail.com>
2026-07-13 08:47:27 +0530

    M2

---
+++ M2/Lesson 2 – Lets Inherit/activity1.java
@@ -0,0 +1,33 @@
+ 
+ 
+    //this activity is mainly to undersand the concept of inheritance
+//Parent Class
+class Parent
+{
+    int age,id;
+    String name;
+    void naming(String name)
+    {
+        System.out.println("Name:"+name);
+    }
+}
+
+//child class
+class Child extends Parent
+{
+    void ageN(int age)
+    {
+        System.out.println("Age of student is:"+age);
+    }
+}
+
+class activity1
+{
+    public static void main(String [] er)
+    {
+        Child s= new Child();//creating object of child class
+        s.naming("Aashish");
+        s.ageN(14);
+    }
+}
+
---
+++ M2/Lesson 4 – Get Set Go/activity1.java
@@ -0,0 +1,21 @@
+
+class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class activity1 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Aashish @Codingal");
+        System.out.println(s.getName());
+    }
+}
+
